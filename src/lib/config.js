dd06ff3e37fad7051a1fd2c51457c6c5548b696c
Feras Hani <122812085+feras-nahhal@users.noreply.github.com>
2025-09-13 21:24:32 +0300

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,5 @@
 export const API_BASE =
-  process.env.NEXT_PUBLIC_API_BASE || "https://smart-expense-tracker-backend.up.railway.app";
-
+  process.env.NEXT_PUBLIC_API_BASE || "http://127.0.0.1:8000";
 export const WS_BASE =
-  process.env.NEXT_PUBLIC_WS_BASE || "wss://smart-expense-tracker-backend.up.railway.app";
+  process.env.NEXT_PUBLIC_WS_BASE || "ws://127.0.0.1:8000";
+
